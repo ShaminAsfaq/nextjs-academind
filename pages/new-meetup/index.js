2aec7b1359bc7550554a3002abb6e5facd92565e
Shamin Asfaq <shamin.asfaq@gmail.com>
2021-10-31 01:05:55 +0600

    Finished Till: https://youtu.be/MFuwkrseXVE?t=4387

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,15 @@
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+const NewMeetupPage = () => {
+    const addMeetupHandler = (meetupData) => {
+        console.log(meetupData);
+    };
+
+    return (
+        <NewMeetupForm onAddMeetup = {addMeetupHandler} />
+    );
+}
+
+export {
+    NewMeetupPage as default
+}
